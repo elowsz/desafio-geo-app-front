74d226e6ec51827bfc0c72592fa5209468600872
Eloisa de Andrade 🌱 <eloisaandrade1006@gmail.com>
2025-11-28 03:03:44 +0000

    Frontend geoapp

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // Substitua pelo SEU IP local do backend (ex: 192.168.0.100)
-  baseURL: 'http://SEU_IP:3000/api', 
+  baseURL: 'http://26.168.161.45:3000/api',
   timeout: 10000,
 });
 
